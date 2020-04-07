f188e7c44f564cad40df317a908dcefb57379846
Anindya Kundu <anindya.k22@outlook.com>
2020-04-07 12:00:39 +0530

    Make dynamic adjustments in scripts

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 $(document).ready(function () {
+    let header_size = $('#header').height();
+    let navbar_size = $('#navbar').height();
+
+    let chairs = $('.chair');
+    for (let i = 0; i < 6; i += 2) {
+        let h1 = $(chairs[i]).height(), h2 = $(chairs[i + 1]).height();
+        let max = h1 > h2 ? h1 : h2;
+
+        $(chairs[i]).css("height", max);
+        $(chairs[i + 1]).css("height", max);
+    }
+
     $(window).resize(function() {
+        header_size = $('#header').height();
+        navbar_size = $('#navbar').height();
+
         if ($('#collbar').height() != 0) {
-            $('#collbar').css('height', $(window).height() - 104);
-            $('#coll-context').css('height', $(window).height() - 104);
+            $('#collbar').css(
+                'height', $(window).height() - (header_size + navbar_size)
+            );
+            $('#coll-context').css(
+                'height', $(window).height() - (header_size + navbar_size)
+            );
         }
     });
 
-    $(window).scroll(function () {
+    $(window).scroll(() => {
         if ($('#collbar').height() != 0) {
             $('#coll-context .card').hide(200);
             setTimeout(() => {
@@ -18,7 +37,7 @@ $(document).ready(function () {
             return;
         }
 
-        if ($(window).scrollTop() >= 40) {
+        if ($(window).scrollTop() >= header_size) {
             $('#navbar').addClass('fixed');
             $('#content-body').css('margin-top', '64px');
             $('nav').css('background', '#333');
@@ -30,9 +49,7 @@ $(document).ready(function () {
             $('nav .logos span').css('color', '#ccc');
             $('nav .nav-links ul li div').css('color', '#ccc');
             $('.active').css('color', '#80ebff');
-        }
-
-        if ($(window).scrollTop() < 40) {
+        } else {
             $('#navbar').removeClass('fixed');
             $('#content-body').css('margin-top', '0');
             $('nav').css('background', 'white');
@@ -46,22 +63,29 @@ $(document).ready(function () {
             $('.active').css('color', '#333');
         }
 
-        var reached = $(window).scrollTop() - 64 + ($(window).height() >> 1);
-        var tops = [];
-        var elems = [ $('#about'), $('#speak'), $('#comm'), $('#trax'), $('#guide'), $('#reg'), $('#foot') ];
-        var links = [ $('#l_about'), $('#l_speak'), $('#l_comm'), $('#l_trax'), $('#l_guide'), $('#l_reg'), $('#foot') ];
+        let reached =
+            $(window).scrollTop() - navbar_size + ($(window).height() >> 1);
+        let sec_tops = [];
+        let sec_elems = [
+            $('#about'), $('#speak'), $('#comm'), $('#trax'),
+            $('#guide'), $('#reg'), $('#foot')
+        ];
+        let sec_links = [
+            $('#l_about'), $('#l_speak'), $('#l_comm'), $('#l_trax'),
+            $('#l_guide'), $('#l_reg'), $('#foot')
+        ];
         
         $('#l_home').removeClass('active');
-        for (var i = 0; i < elems.length; i++) {
-            tops[i] = elems[i].position().top;
-            links[i].removeClass('active');
+        for (let i = 0; i < sec_elems.length; i++) {
+            sec_tops[i] = sec_elems[i].position().top;
+            sec_links[i].removeClass('active');
         }
 
-        var index = -1;
-        for (var i = 0; i < tops.length - 1; i++) {
-            if (reached < tops[0])
+        let index = -1;
+        for (let i = 0; i < sec_tops.length - 1; i++) {
+            if (reached < sec_tops[0])
                 break;
-            if (reached >= tops[i] && reached < tops[i + 1]) {
+            if (reached >= sec_tops[i] && reached < sec_tops[i + 1]) {
                 index = i;
                 break;
             }
@@ -70,9 +94,9 @@ $(document).ready(function () {
         if (index == -1)
             $('#l_home').addClass('active');
         else
-            links[index].addClass('active');
+            sec_links[index].addClass('active');
         
-        if ($(window).scrollTop() > 64)
+        if ($(window).scrollTop() >= header_size)
             $('#btn-top').css({
                 "opacity":".75",
                 "visibility":"visible",
@@ -85,122 +109,88 @@ $(document).ready(function () {
         });
     });
 
-    var countDownDate = new Date("Mar 15, 2021 11:00:00").getTime();
-    var setCountdown = function () {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    // Count Down
+    let countDownDate = new Date("Mar 15, 2021 11:00:00").getTime();
+    let setCountdown = function () {
+        let now = new Date().getTime();
+        let dif = countDownDate - now;
+
+        let days = Math.floor(dif / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
 
         document.getElementById('days').innerHTML = makeDoubleDigit(days);
         document.getElementById('hours').innerHTML = makeDoubleDigit(hours);
         document.getElementById('mins').innerHTML = makeDoubleDigit(mins);
 
-        if (distance < 0)
-            clearInterval(x);
+        if (dif < 0)
+            clearInterval(countdown);
     };
 
-    var makeDoubleDigit = num => {
+    let makeDoubleDigit = num => {
         return (num.toString().length > 1 ? num : "0" + num);
     }
 
     setCountdown();
-    var x = setInterval(function () {
+    let countdown = setInterval(() => {
         setCountdown();
     }, 60000);
 
-    $('#scroll-btn').click(() => {
-        var top = $('#info').position().top - 96;
+
+    // Click Events
+    $('#scroll-btn').click(
+        () => smoothScrollTo($('#info').position().top - 96)
+    );
+
+    $('#l_home').click(() => smoothScrollTo(0));
+
+    $('#l_about').click(
+        () => smoothScrollTo($('#about').position().top - 64)
+    );
+
+    $('#l_speak').click(
+        () => smoothScrollTo($('#speak').position().top - 64)
+    );
+
+    $('#l_comm').click(
+        () => smoothScrollTo($('#comm').position().top - 64)
+    );
+
+    $('#l_trax').click(
+        () => smoothScrollTo($('#trax').position().top - 64)
+    );
+
+    $('#l_guide').click(
+        () => smoothScrollTo($('#guide').position().top - 64)
+    );
+
+    $('#l_reg').click(
+        () => smoothScrollTo($('#reg').position().top - 64)
+    );
+
+    $('#btn-top').click(() => smoothScrollTo(0));
+
+    let smoothScrollTo = top => {
         window.scrollTo({
             top: top,
             behavior: 'smooth'
         });
-    });
-
-    $('#l_home').click(() => {
-        var top = 0;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
-
-    $('#l_about').click(() => {
-        var top = $('#about').position().top - 64;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
-
-    $('#l_speak').click(() => {
-        var top = $('#speak').position().top - 64;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
-
-    $('#l_comm').click(() => {
-        var top = $('#comm').position().top - 64;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
-
-    $('#l_trax').click(() => {
-        var top = $('#trax').position().top - 64;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
-
-    $('#l_guide').click(() => {
-        var top = $('#guide').position().top - 64;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
-
-    $('#l_reg').click(() => {
-        var top = $('#reg').position().top - 64;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
-
-    var chairs = $('.chair');
-    for (var i = 0; i < 6; i += 2) {
-        var h1 = $(chairs[i]).height(), h2 = $(chairs[i + 1]).height();
-        var max = h1 > h2 ? h1 : h2;
-
-        $(chairs[i]).css("height", max);
-        $(chairs[i + 1]).css("height", max);
-    }
-
-    $('#btn-top').click(() => {
-        var top = 0;
-        window.scrollTo({
-            top: top,
-            behavior: 'smooth'
-        });
-    });
+    };
 
     $('#coll-context .card').hide();
     var collmenu = -1;
     var c_links = [ $('#venue'), $('#pconf'), $('#spons'), $('#contc') ];
     var cards = [ $('#c_ven'), $('#c_prv'), $('#c_spn'), $('#c_con') ];
-    var toggleCollapseBar = function(i) {
+    var toggleCollapseBar = i => {
         $('.header-links ul li div').css('color', '#666');
         if ($('#collbar').height() === 0) {
-            $('#collbar').css('height', $(window).height() - 104);
-            $('#coll-context').css('height', $(window).height() - 104);
+            $('#collbar').css(
+                'height', $(window).height() - (header_size + navbar_size)
+            );
+            $('#coll-context').css(
+                'height', $(window).height() - (header_size + navbar_size)
+            );
             setTimeout(() => {
                 cards[i].show(200);
             }, 250);
@@ -234,19 +224,8 @@ $(document).ready(function () {
             }
         );
 
-    $('#venue').click(() => {
-        toggleCollapseBar(0);
-    });
-
-    $('#pconf').click(() => {
-        toggleCollapseBar(1);
-    });
-
-    $('#spons').click(() => {
-        toggleCollapseBar(2);
-    });
-
-    $('#contc').click(() => {
-        toggleCollapseBar(3);
-    });
+    $('#venue').click(() => toggleCollapseBar(0));
+    $('#pconf').click(() => toggleCollapseBar(1));
+    $('#spons').click(() => toggleCollapseBar(2));
+    $('#contc').click(() => toggleCollapseBar(3));
 });
