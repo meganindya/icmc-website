f6ada7d106e1cc2560bba06ecd7bf419b9d602df
Anindya Kundu <anindya.k22@outlook.com>
2020-11-07 01:26:02 +0530

    Update cleanup plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
 
 module.exports = {
     entry: './src/js/app.js',
@@ -67,8 +67,16 @@ module.exports = {
                 removeAttributeQuotes: true
             }
         }),
-        new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns: ['dist']
+        new RemovePlugin({
+            before: {
+                test: [
+                    {
+                        folder: './dist',
+                        method: () => true
+                    }
+                ],
+                exclude: ['./dist/.git']
+            }
         })
     ],
     devServer: {
