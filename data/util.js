93792536bf0400b78c242d969bc437c1125234d5
Anindya Kundu <anindya.k22@outlook.com>
2021-01-05 17:29:24 +0530

    Update names

--- data/util.js
+++ data/util.js
@@ -7,12 +7,12 @@ fs.writeFile('./math.txt', '', () => {});
 
 const comp = _comp.sort((a, b) => a.name.localeCompare(b.name));
 let compStr = '';
-comp.forEach(item => compStr += `                <p>\n                  <strong>${item.name}</strong>, ${item.institute}\n                </p>\n`);
+comp.forEach(item => compStr += `                  <p>\n                    <strong>${item.name}</strong>, ${item.institute}\n                  </p>\n`);
 fs.appendFile('./comp.txt', compStr, () => {});
 
 const math = _math.sort((a, b) => a.name.localeCompare(b.name));
 let mathStr = '';
-math.forEach(item => mathStr += `                <p>\n                  <strong>${item.name}</strong>, ${item.institute}\n                </p>\n`);
+math.forEach(item => mathStr += `                  <p>\n                    <strong>${item.name}</strong>, ${item.institute}\n                  </p>\n`);
 fs.appendFile('./math.txt', mathStr, () => {});
 
 // add "type": "module" in package.json, then run with `node --experimental-json-modules util.js`
